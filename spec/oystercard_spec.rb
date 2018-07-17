f4ab8d553c9435090f8629d499b2d120ae5b2a97
edthomas93 <et1270@my.bristol.ac.uk>
2018-07-17 09:08:20 +0100

    Top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'responds to balance' do
-    expect(subject.balance).to eq 0
+  describe '#balance' do
+    it 'responds to balance' do
+      expect(subject.balance).to respond_to
+    end
+  end
+
+  describe '#top_up' do
+    it 'responds to top_up' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it 'can top up the balance' do
+      expect { subject.top_up 1 }.to change{ subject.balance }.by 1
   end
 end
